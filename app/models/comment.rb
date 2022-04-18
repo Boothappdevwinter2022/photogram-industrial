1fb95d6953242094be666a7a7c9eebb15b5914c4
thaong91 <tnguye13@chicagobooth.edu>
2022-04-18 04:32:34 +0000

    Added counter caches

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,13 +3,23 @@
 # Table name: comments
 #
 #  id         :bigint           not null, primary key
-#  author_id  :bigint           not null
-#  photo_id   :bigint           not null
 #  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  author_id  :bigint           not null
+#  photo_id   :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_author_id  (author_id)
+#  index_comments_on_photo_id   (photo_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (author_id => users.id)
+#  fk_rails_...  (photo_id => photos.id)
 #
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: "User", counter_cache: true # for this counter_cache to work, we have to have a column in the Users table for comments_count
   belongs_to :photo
 end
