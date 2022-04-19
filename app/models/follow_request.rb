f4f36406d5a12bca6f3618290539347086282054
thaong91 <tnguye13@chicagobooth.edu>
2022-04-19 01:19:12 +0000

    Added enum to FollowRequest model

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -22,5 +22,11 @@
 class FollowRequest < ApplicationRecord
   belongs_to :recipient, class_name: "User"
   belongs_to :sender, class_name: "User"
+  
+  enum status: { pending: "pending", rejected: "rejected", accepted: "accepted" }
+  # Line 26 is equal to:
+  # scope :accepted, -> { where(status: "accepted")}
+  # and
+  # scope :accepted, -> { where.not(status: "accepted")}, etc.
 
 end
