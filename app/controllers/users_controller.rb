4372a19b7fbb2fcef8417ca2c7ad52ba39c30de5
thaong91 <tnguye13@chicagobooth.edu>
2022-04-19 16:03:46 +0000

    Set up users#show action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find_by!(username: params.fetch(:id)) # use find_by! so that it will return an error if cannot find user instead of returning a nil
+  end
+end
